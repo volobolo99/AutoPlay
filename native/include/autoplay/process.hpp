e633ec2e723058b4b69790f8292a7ef8cbb4d7df
volobolo99 <volobolo99@gmail.com>
2026-09-02 21:44:31 +0200

    Expose native process handle for module enumeration

--- native/include/autoplay/process.hpp
+++ native/include/autoplay/process.hpp
@@ -2,7 +2,6 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <string>
 #include <vector>
 
 namespace autoplay::native {
@@ -12,38 +11,24 @@ public:
     Process() = default;
     explicit Process(std::uint32_t pid);
     ~Process();
-
     Process(const Process&) = delete;
     Process& operator=(const Process&) = delete;
-
     bool open(std::uint32_t pid, bool write_access = false);
     void close();
     bool valid() const noexcept;
     std::uint32_t pid() const noexcept;
-
+    void* native_handle() const noexcept { return handle_; }
     bool read(std::uintptr_t address, void* out, std::size_t size) const;
     bool write(std::uintptr_t address, const void* data, std::size_t size) const;
-
-    template <typename T>
-    bool read(std::uintptr_t address, T& out) const { return read(address, &out, sizeof(T)); }
-
-    template <typename T>
-    bool write(std::uintptr_t address, const T& value) const { return write(address, &value, sizeof(T)); }
-
+    template <typename T> bool read(std::uintptr_t address, T& out) const { return read(address, &out, sizeof(T)); }
+    template <typename T> bool write(std::uintptr_t address, const T& value) const { return write(address, &value, sizeof(T)); }
 private:
     void* handle_{nullptr};
     std::uint32_t pid_{0};
     bool write_access_{false};
 };
 
-struct MemoryRegion {
-    std::uintptr_t base{0};
-    std::size_t size{0};
-    std::uint32_t protection{0};
-    std::uint32_t state{0};
-    std::uint32_t type{0};
-};
-
+struct MemoryRegion { std::uintptr_t base{0}; std::size_t size{0}; std::uint32_t protection{0}; std::uint32_t state{0}; std::uint32_t type{0}; };
 std::vector<MemoryRegion> readable_regions(const Process& process);
 
 } // namespace autoplay::native
